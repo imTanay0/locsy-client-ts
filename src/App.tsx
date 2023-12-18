c7a0d635e0b1be41a3f6610718732d35f2b5eb9a
imtanay <107740662+imTanay0@users.noreply.github.com>
2023-12-18 21:33:33 +0530

    Initialize project with tailwind and shadcn/ui library

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="font-bold text-4xl">Hello</h1>
+      <Button >Click here</Button>
+    </>
+  )
+}
+
+export default App
