d6311c364e5c490ed672e6e3b06e3346e184851d
imtanay <107740662+imTanay0@users.noreply.github.com>
2024-03-31 22:03:03 +0530

    Set up redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
 import App from "./App.tsx";
-// import { Toaster } from "@/components/ui/sonner";
 import { Toaster } from "sonner";
 import "./index.css";
+import { store } from "@/redux/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-    <Toaster richColors />
+    <Provider store={store}>
+      <App />
+      <Toaster richColors />
+    </Provider>
   </React.StrictMode>
 );
