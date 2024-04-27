24bc9144ccbe33fafcd5101df9dbb0366057e025
imtanay <107740662+imTanay0@users.noreply.github.com>
2024-04-27 20:46:58 +0530

    Clean up

--- src/pages/Checkout/CheckoutPage.tsx
+++ src/pages/Checkout/CheckoutPage.tsx
@@ -1,27 +1,9 @@
-// import { useState } from "react";
-// import { useLocation } from "react-router-dom";
-
 import Stepper from "@/components/stepper";
-// import OrderSummary from "./OrderSummary";
-// import DeliveryAddressForm from "./DeliveryAddressForm";
 
 const CheckoutPage = () => {
-  // const location = useLocation();
-  // const querySearch = new URLSearchParams(location.search);
-
-  // const stepQuery = querySearch.get("step");
-  // const step = stepQuery ? parseInt(stepQuery) : 1;
-
-  // const [currentStep, setCurrentStep] = useState(step);
-
-  // const handleStepChange = (newStep: number) => {
-  //   setCurrentStep(newStep);
-  // };
-
   return (
     <div className="min-h-[100svh] px-4 md:px-6 py-12 container">
       <Stepper />
-      {/* <Stepper step={step} onStepChange={handleStepChange} /> */}
     </div>
   );
 };
